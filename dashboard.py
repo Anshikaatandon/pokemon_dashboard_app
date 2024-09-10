2e9b1d8d54bd1f7e39bd89d7b1fd747563566269
anshikatandon <anshikatandon02@gmail.com>
2024-09-10 15:27:03 +0530

    first commit

---
+++ dashboard.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+st.set_page_config(
+    layout='wide',
+    page_icon="🎉",
+    page_title="Pokemon Dashboard"
+)
+
+@st.cache_data
+def load_data():
+    file="pokemon.csv"
+    data=pd.read_csv(file)
+    return data
+
+def main():
+    st.image("hero_image.jpg", use_column_width=True)
+    st.title("pokemon dashboard")
+    with st.spinner("Loading Pokemons..."):
+        df = load_data()
+        # st.snow()
+    rows,columns=df.shape
+    col_names = df.columns.tolist()
+
+    c1,c2,c3 = st.columns(3)
+    c1.subheader(f"Total Rows:{rows}")
+    c2.subheader(f"Total Columns:{columns}")
+    c3.subheader(f"Columns:{",".join(col_names)}")
+
+    #st.write(df.head())
+                
+   
+
+
+if __name__=="__main__":
+    main()
